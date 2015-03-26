57160806207fddd480d787fe5ab17595fdc522c2
srinivasvaranasi123 <srinivas13mca@gmail.com>
2015-03-26 21:24:06 +0530

    WebapiApplicationExample

---
+++ Webapiloginapp1/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Webapiloginapp1.Models;
+
+namespace Webapiloginapp1.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private codeitEntities db = new codeitEntities();
+
+        [HttpPost]
+        public string LoginUser(UserRegistration obj)
+        {
+
+            
+            var objuser = db.UserRegistrations.FirstOrDefault(p => p.UserName == obj.UserName && p.Password == obj.Password);
+            //  var product = products.FirstOrDefault((p) => p.Id == id);
+            if (objuser == null)
+            {
+                return "Login Failed";
+            }
+            return "Login Suceess";
+
+        }
+        
+    }
+}
